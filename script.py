f8f1d83761734ca78be2a7641bfc4968a0af3f01
Gustavo Scharf <scharfgustavo@gmail.com>
2020-04-24 22:14:48 -0400

    added methods to get raw info and transform in python dicts

--- script.py
+++ script.py
@@ -4,22 +4,27 @@ class Handler():
 	def __init__(self,fileName):
 		### takes in import file
 		self.fileName = fileName
-		self.loadFile(fileName)
-
-	def loadFile(self,fileName):
 		self.wb = load_workbook(fileName)
-
-	def getSheetNames(self):
-		sheetNames = self.wb.sheetnames
-		print(sheetNames)
+		self.sheetNames = self.wb.sheetnames
 
 	def getRecordsFromSheet(self,sheetName):
-		print(self.wb[sheetName]['B3'].value)
-		x = self.findEdge(sheetName)
-		print(x)
-
-	def findEdge(self,sheetName):
 		sheet = self.wb[sheetName]
+		edge = self.findEdge(sheet)
+
+		rows = []
+		
+		cols = ['A','B','C','D','E','F','G','H']
+
+		for x in range(2,edge):
+			row = []
+			for y in cols:
+				loc = y+str(x)
+				row.append(sheet[loc].value)
+			rows.append(row)
+
+		return rows
+
+	def findEdge(self,sheet):
 		counter = 1
 		while True:
 			if sheet['A'+str(counter)].value == None:
@@ -28,7 +33,30 @@ class Handler():
 				counter += 1
 		return counter
 
+	def initMainDict(self):
+		canvNames = self.sheetNames[2:]
+		self.mainDict = {}
+		for name in canvNames:
+			rows = self.getRecordsFromSheet(name)
+			self.mainDict[name] = rows
+
+	def initExtraDict(self):
+		self.extraDict = {}
+		sheet = self.wb[self.sheetNames[0]]
+		edge = self.findEdge(sheet)
+
+		cols = ['B','D','F']
+		rows = []
+		for x in range(1,edge):
+			row = []
+			for y in cols:
+				loc = y+str(x)
+				row.append(sheet[loc].value)
+			rows.append(row)
+		print(rows)
+
 
 handler = Handler('4-24-2020.xlsx')
-handler.getSheetNames()
-handler.getRecordsFromSheet('Abreu, Rosaura')
+# handler.getRecordsFromSheet('Abreu, Rosaura')
+# handler.initMainDict()
+handler.initExtraDict()
